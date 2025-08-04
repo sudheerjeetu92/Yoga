e73323b35c0f1cd1f41d1f2da51d29a17b31636b
Your Name <sudheerjeetu92@email.com>
2025-08-04 11:15:21 +0530

    Added client folder

---
+++ client/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontsize: {
+        "course-details-heading-small": ["26px", "36px"],
+        "course-details-heading-large": ["36px", "44px"],
+        "home-heading-small": ["28px", "34px"],
+        "home-heading-large": ["28px", "34px"],
+        default: ["15px", "21px"],
+      },
+      gridTemplateColumns: {
+        "auto": "repeat(auto-fit, minmax(200px,1fr))",
+      },
+    },
+  },
+  plugins: [],
+};
