ee88136eb3c817651b377188be4f2e91d0eebbcf
Your Name <sudheerjeetu92@email.com>
2025-08-08 17:48:39 +0530

    initial comment

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -11,6 +11,7 @@ export const AppContextProvider = (props) => {
 
   const [allCourses, setAllCourses] = useState([]);
   const [isEducator, setIsEducator] = useState(true);
+  const [enrolledCourses, setEnrolledCourses] = useState([]);
 
   //Fetch all courses
   const fetchAllCourses = async () => {
@@ -55,8 +56,14 @@ export const AppContextProvider = (props) => {
     return totalLectures;
   };
 
+  //Fetch user Enrolled Coures
+  const fetchUserEnrolledCourses = async () => {
+    setEnrolledCourses(dummyCourses);
+  };
+
   useEffect(() => {
     fetchAllCourses();
+    fetchUserEnrolledCourses();
   }, []);
 
   const value = {
@@ -69,6 +76,7 @@ export const AppContextProvider = (props) => {
     calculateChapterTime,
     calculateCourseDuration,
     calculateNoOfLecture,
+    enrolledCourses,fetchUserEnrolledCourses
   };
 
   return (
